698243096b423978d374d14ffd488230f5e274e9
Kai Krakow <hurikhan77@gmail.com>
2014-05-12 19:12:48 +0200

    Apply the real fix for Rails 4+
    I had all files duplicated by my previous change. The problem is more like "assets.enabled" is always nil in Rails 4.

--- lib/rack-offline.rb
+++ lib/rack-offline.rb
@@ -25,16 +25,17 @@ module Rails
 
     def cache_block(root)
       Proc.new do
-        if Rails.version >= "3.1" && Rails.configuration.assets.enabled
+        if (Rails.version >= "3.1" && Rails.configuration.assets.enabled) ||
+           (Rails.version >= "4.0" && !Rails.configuration.assets.compile)
           files = Dir[
-            "#{root}/{**/,}*.html",
-            "#{root}/assets/{**/,}*.{js,css,jpg,png,gif}"]
+            "#{root}/**/*.html",
+            "#{root}/assets/**/*.{js,css,jpg,png,gif}"]
         else
           files = Dir[
-            "#{root}/{**/,}*.html",
-            "#{root}/stylesheets/{**/,}*.css",
-            "#{root}/javascripts/{**/,}*.js",
-            "#{root}/images/{**/,}*.*"]
+            "#{root}/**/*.html",
+            "#{root}/stylesheets/**/*.css",
+            "#{root}/javascripts/**/*.js",
+            "#{root}/images/**/*.*"]
         end
         
         files.each do |file|
